e85c6eafbd3ba3374191c049d27a8e0eb8bf1226
Darya Makeeva <dasha.makeeva.99@mail.ru>
2023-09-26 18:51:55 +0300

    first commit

---
+++ src/_config.js
@@ -0,0 +1,15 @@
+//файл с константами
+
+export const app = {
+    title: "Notes"
+};
+export const links = [
+    {
+        title: "Главная",
+        url: "/",
+    },
+    {
+        title: "О сайте",
+        url: "/about",
+    },
+];
